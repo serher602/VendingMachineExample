1ba04a73ac9c7c85c20e5b1481fd200055f0b22e
serher602 <serher602@gmail.com>
2019-04-17 20:58:24 -0500

    Add files via upload

---
+++ Proj3-Hernandez/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proj3_Hernandez
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        // Instantiate VendingMachine object.
+        VendingMachine vm = new VendingMachine();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtMoney.Text = (vm.NumQuarters * .25 ).ToString("C");
+            txtTwixAmt.Text = vm.TwixBar.ToString();
+            txtKitkatAmt.Text = vm.KitkatBar.ToString();
+            txtHersheysAmt.Text = vm.HersheysBar.ToString();
+            txtReesesAmt.Text = vm.ReesesCups.ToString();
+            txtTotalVended.Text = vm.NumCandyBars.ToString();
+
+
+
+
+        }
+
+        // 4 click events for every bar that update the text boxes around them, including
+        // the amount of money, the number of bars dispensed, and the number of each candy bar
+        private void btnTwix_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (vm.NumQuarters >= 3 && vm.TwixBar > 0)
+            {
+                vm.SelectTwix();
+                MessageBox.Show("Twix Bar Dispensed");
+            }
+            else if (vm.TwixBar > 0)
+            {
+                MessageBox.Show("Not enough quarters to buy a Twix bar.");
+            }
+            else
+            {
+                MessageBox.Show("No Twixs bars in machine.");
+            }
+
+            txtTwixAmt.Text = vm.TwixBar.ToString();
+            txtMoney.Text = (vm.NumQuarters * .25).ToString("C");
+            txtTotalVended.Text = vm.NumCandyBars.ToString();
+        }
+
+        private void btnKitkat_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (vm.NumQuarters >= 4 && vm.KitkatBar > 0)
+            {
+                vm.SelectKitkat();
+                MessageBox.Show("KitKat Bar Dispensed");
+            }
+            else if (vm.KitkatBar > 0)
+            {
+                MessageBox.Show("Not enough quarters to buy a KitKat bar.");
+            }
+            else
+            {
+                MessageBox.Show("No KitKat bars in machine.");
+            }
+
+            txtKitkatAmt.Text = vm.KitkatBar.ToString();
+            txtMoney.Text = (vm.NumQuarters * .25).ToString("C");
+            txtTotalVended.Text = vm.NumCandyBars.ToString();
+
+        }
+
+        private void btnHersheys_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (vm.NumQuarters >= 5 && vm.HersheysBar > 0)
+            {
+                vm.SelectHersheys();               
+                MessageBox.Show("Hershey's Bar Dispensed");
+            }
+            else if (vm.HersheysBar > 0)
+            {
+                MessageBox.Show("Not enough quarters to buy a Hershey's bar.");
+            }
+            else
+            {
+                MessageBox.Show("No Hershey's bars in machine.");
+            }
+
+            txtHersheysAmt.Text = vm.HersheysBar.ToString();
+            txtMoney.Text = (vm.NumQuarters * .25).ToString("C");
+            txtTotalVended.Text = vm.NumCandyBars.ToString();
+        }
+
+        private void btnReeses_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (vm.NumQuarters >= 6 && vm.ReesesCups > 0)
+            {
+                vm.SelectReeses();
+                MessageBox.Show("Reese's Cups Dispensed");
+            }
+            else if (vm.ReesesCups > 0)
+            {
+                MessageBox.Show("Not enough quarters to buy Reese's Cups.");
+            }
+            else
+            {
+                MessageBox.Show("No Reese's Cups in machine.");
+            }
+
+            txtReesesAmt.Text = vm.ReesesCups.ToString();
+            txtMoney.Text = (vm.NumQuarters * .25).ToString("C");
+            txtTotalVended.Text = vm.NumCandyBars.ToString();
+        }
+
+
+        //Deposit Quarter Method that update the text box next to it
+        private void btnDeposit_Click(object sender, RoutedEventArgs e)
+        {
+            vm.DepositQuarter();
+            txtMoney.Text = (vm.NumQuarters * .25).ToString("C");       
+
+        }
+    }
+}
---
+++ Proj3-Hernandez/Proj3-VendingMachineExampleEdited.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj3_Hernandez
+{
+    /// <summary>
+    /// Simulate a candy bar vending machine that starts with
+    /// 5 candy bars, where each candy bar costs 3 quarters.
+    /// </summary>
+
+    public class VendingMachine
+    {
+        // private instance variables
+        private int numQuarters, numCandyBars, twixBar, kitkatBar, hersheysBar, reesesCups;
+
+        // Public noarg constructor
+        public VendingMachine()
+        {
+            // Set the candy bar cost in quarters.
+            numQuarters = 3;
+
+            // Set initial number of candy bars.
+            twixBar = 2;
+            kitkatBar = 2;
+            hersheysBar = 2;
+            reesesCups = 2;
+            numCandyBars = 0;
+        }
+
+        // Public read only properties:
+
+        public int NumQuarters
+        {
+            get { return numQuarters; }
+        }
+
+        public int NumCandyBars
+        {
+            get { return numCandyBars; }
+        }
+
+        public int TwixBar
+        {
+            get { return twixBar; }
+        }
+
+        public int KitkatBar
+        {
+            get { return kitkatBar; }
+        }
+        public int HersheysBar
+        {
+            get { return hersheysBar; }
+        }
+        public int ReesesCups
+        {
+            get { return reesesCups; }
+        }
+
+        // Public methods:
+
+        public string DepositQuarter()
+        {
+            numQuarters++;
+            return "Quarter deposited.";
+        }
+
+
+        // 4 Different Select Methods for each candy bar
+        public string SelectTwix()
+        {
+            if (numQuarters >= 3 && twixBar > 0)
+            {
+                numQuarters = numQuarters - 3;
+                twixBar = twixBar - 1;
+                numCandyBars = numCandyBars +  1;
+                return "Twix bar dispensed";
+            }
+            else if (twixBar > 0)
+            {
+                return "Not enough quarters to buy a Twix bar.";
+            }
+            else
+            {
+                return "No Twix bars in machine.";
+            }
+
+        }
+
+        public string SelectKitkat()
+        {
+            if (numQuarters >= 4 && kitkatBar > 0)
+            {
+                numQuarters = numQuarters - 4;
+                kitkatBar = kitkatBar - 1;
+                numCandyBars = numCandyBars + 1;
+                return "KitKat bar dispensed";
+            }
+            else if (kitkatBar > 0)
+            {
+                return "Not enough quarters to buy a KitKat bar.";
+            }
+            else
+            {
+                return "No KitKat bars in machine.";
+            }
+
+        }
+
+        public string SelectHersheys()
+        {
+            if (numQuarters >= 5 && hersheysBar > 0)
+            {
+                numQuarters = numQuarters - 5;
+                hersheysBar = hersheysBar - 1;
+                numCandyBars = numCandyBars + 1;
+                return "Hershey's bar dispensed";
+            }
+            else if (hersheysBar > 0)
+            {
+                return "Not enough quarters to buy a Hershey's bar.";
+            }
+            else
+            {
+                return "No Hershey's bars in machine.";
+            }
+
+        }
+
+        public string SelectReeses()
+        {
+            if (numQuarters >= 6 && reesesCups > 0)
+            {
+                numQuarters = numQuarters - 6;
+                reesesCups = reesesCups - 1;
+                numCandyBars = numCandyBars + 1;
+                return "Reese's Cups dispensed";
+            }
+            else if (reesesCups > 0)
+            {
+                return "Not enough quarters to buy a Reese's Cups.";
+            }
+            else
+            {
+                return "No Reese's Cups in machine.";
+            }
+
+        }
+
+
+        // ToString Method
+        public override string ToString()
+        {
+
+            return "Total Quarters: " + numQuarters + " Total Bars:" + numCandyBars +
+             " Twix Bars:" + twixBar + " KitKat Bars:" + kitkatBar + " Hershey Bars:"
+            + hersheysBar + " Reese's Cups:" + reesesCups;
+        }
+    }
+
+}
